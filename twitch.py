5247ecce78febd649d0ab165ab2b9bd6fb4bcf0f
Andrii <andrij.mykytuk@gmail.com>
2023-04-25 18:58:34 +0300

    wip

--- twitch.py
+++ twitch.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,13 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Twitch:
     def __init__(self):
-        options = ChromeOptions()
-        options.add_argument("no-sandbox")
-        options.accept_untrusted_certs = True
-        options.assume_untrusted_cert_issuer = True
-        options.add_argument("--disable-infobars")
-        options.add_argument("--headless")
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        self.driver = webdriver.Remote(
+            command_executor='http://chrome:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
 
     def get_followers_count(self, username):
         url = f"https://www.twitch.tv/{username}"
@@ -23,8 +18,3 @@ class Twitch:
         wait = WebDriverWait(self.driver, 25)
         followers = wait.until(EC.presence_of_element_located((By.XPATH, '//p[contains(text(), "followers")]')))
         return followers.text.replace(' followers', '')
-
-
-
-# t = Twitch()
-# print(t.get_followers_count('seba'))
