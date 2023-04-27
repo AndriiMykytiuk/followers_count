4b947a513312ab292498658435e2e8979ee3b346
Andrii <andrij.mykytuk@gmail.com>
2023-04-27 18:58:59 +0300

    wip

--- twitch.py
+++ twitch.py
@@ -15,6 +15,7 @@ class Twitch:
         options.add_argument("--window-size=1280x1696")
         options.add_argument("--single-process")
         options.add_experimental_option('detach', True)
+        options.add_argument('--disable-gpu')
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
