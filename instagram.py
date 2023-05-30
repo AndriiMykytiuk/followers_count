589d6cd72da4afeb441887eb519b669cc77e5d8e
Andrii <andrij.mykytuk@gmail.com>
2023-05-30 11:28:22 +0300

    wip

--- instagram.py
+++ instagram.py
@@ -5,7 +5,7 @@ class Instagram:
     def __init__(self):
         USERNAME = "PUT YOUR USERNAME HERE"
         self.instaloader = Instaloader()
-        self.instaloader.load_session_from_file(USERNAME, filename='session-andrii_garden_experience')
+        self.instaloader.load_session_from_file(USERNAME, filename='<filename>')
 
     def get_followers(self, username):
         try:
